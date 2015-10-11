cecc9bf2c3fa47634290c5848eac1f3b6af4b1b6
Ian M <ian.w.mcginnis@gmail.com>
2015-10-11 00:18:29 -0600

    Adding initial rss script for parsing Reuters feeds

---
+++ rss/rss.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import feedparser
+
+#TODO: function that validates/normalizes the 'location' field. Right now it's arbitrary. (values have been like: "Hawaii", "San Antonio", "Polk City, Fla", "SACRAMENTO, Calif./LOS ANGELES" and "" )
+
+def getNews():
+    """Gets news from reuters domestic feed. Stores summary, link & location for each story.
+
+    Returns a list of dictionaries of current new entries."""
+
+    reuters = feedparser.parse("http://feeds.reuters.com/Reuters/domesticNews")
+    entries = []
+    for entry in reuters['entries']:
+        entry_dict = {
+        'summary': entry['summary'].split('<br',1)[0],
+        'link': entry['link'],
+        'location': entry['summary'].split('(Reuters)',1)[0],
+        }
+        entries.append(entry_dict)
+    return entries
+
+for entry in getNews():
+    print entry
