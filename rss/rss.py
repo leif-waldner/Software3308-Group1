c7064f5a9a411ba683f5c58d868948d68e7b47fa
Ian M <ian.w.mcginnis@gmail.com>
2015-10-11 00:32:56 -0600

    Adding title to rss parsing

--- rss/rss.py
+++ rss/rss.py
@@ -5,7 +5,7 @@ import feedparser
 #TODO: function that validates/normalizes the 'location' field. Right now it's arbitrary. (values have been like: "Hawaii", "San Antonio", "Polk City, Fla", "SACRAMENTO, Calif./LOS ANGELES" and "" )
 
 def getNews():
-    """Gets news from reuters domestic feed. Stores summary, link & location for each story.
+    """Gets news from reuters domestic feed. Stores title, summary, link & location for each story.
 
     Returns a list of dictionaries of current new entries."""
 
@@ -13,6 +13,7 @@ def getNews():
     entries = []
     for entry in reuters['entries']:
         entry_dict = {
+        'title': entry['title'],
         'summary': entry['summary'].split('<br',1)[0],
         'link': entry['link'],
         'location': entry['summary'].split('(Reuters)',1)[0],
