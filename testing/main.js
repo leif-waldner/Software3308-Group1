7814ae993e2cf458472fab912408ba77ce66af97
Taylor Maurer <taylor.maurer@colorado.edu>
2015-12-06 20:44:14 -0700

    Moved testing stuff into a testing directory for easier access.

---
+++ testing/main.js
@@ -0,0 +1,79 @@
+function showRSS(str) {
+  if (str.length==0) {
+    document.getElementById("rssOutput").innerHTML="";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById("rssOutput").innerHTML=xmlhttp.responseText;
+    }
+  }
+  //can add test for correct getrss.php?q=STATENAME
+  xmlhttp.open("GET","getrss.php?q="+str,true);
+  xmlhttp.send();
+}
+
+function getName(abbr) {
+  var ret = states[abbr];
+  return ret;
+}
+
+var states = {
+    'AL':'Alabama',
+    'AK':'Alaska',
+    'AZ':'Arizona',
+    'AR':'Arkansas',
+    'CA':'California',
+    'CO':'Colorado',
+    'CT':'Connecticut',
+    'DE':'Delaware',
+    'DC':'District of Columbia',
+    'FL':'Florida',
+    'GA':'Georgia',
+    'HI':'Hawaii',
+    'ID':'Idaho',
+    'IL':'Illinois',
+    'IN':'Indiana',
+    'IA':'Iowa',
+    'KS':'Kansas',
+    'KY':'Kentucky',
+    'LA':'Louisiana',
+    'ME':'Maine',
+    'MD':'Maryland',
+    'MA':'Massachusetts',
+    'MI':'Michigan',
+    'MN':'Minnesota',
+    'MS':'Mississippi',
+    'MO':'Missouri',
+    'MT':'Montana',
+    'NE':'Nebraska',
+    'NV':'Nevada',
+    'NH':'New Hampshire',
+    'NJ':'New Jersey',
+    'NM':'New Mexico',
+    'NY':'New York',
+    'NC':'North Carolina',
+    'ND':'North Dakota',
+    'OH':'Ohio',
+    'OK':'Oklahoma',
+    'OR':'Oregon',
+    'PA':'Pennsylvania',
+    'RI':'Rhode Island',
+    'SC':'South Carolina',
+    'SD':'South Dakota',
+    'TN':'Tennessee',
+    'TX':'Texas',
+    'UT':'Utah',
+    'VT':'Vermont',
+    'VA':'Virginia',
+    'WA':'Washington',
+    'WV':'West Virginia',
+    'WI':'Wisconsin',
+    'WY':'Wyoming',
+};
